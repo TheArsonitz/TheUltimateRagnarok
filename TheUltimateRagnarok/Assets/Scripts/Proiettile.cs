a59d1bc1c9bcff10f02b440ed0ca19108ceb70f4
Lorenzo Liseni <lorenzo1@MacBookAir.fritz.box>
2026-03-06 12:24:21 +0100

    msg

---
+++ TheUltimateRagnarok/Assets/Scripts/Proiettile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proiettile : MonoBehaviour
+{
+
+    private GameObject proprietario;
+    private Rigidbody2D rb;
+
+    [Header("Stats colpo")]
+    public float velocita = 0.0f;
+    public float danno = 0.0f;
+
+    [Header("Settings Collisioni")]
+    public LayerMask layerMuri;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Lancia(float direzione, GameObject chiHaSparato) {
+
+        proprietario = chiHaSparato;
+
+        if(rb == null) rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(velocita * direzione, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.gameObject == proprietario) return;
+
+        HealthSystem vita = collision.GetComponent<HealthSystem>();
+        if(vita != null) {
+            vita.PrendiDanno(danno);
+            DistruggiProiettile();
+            return;
+        }
+
+        if ((layerMuri.value & (1 << collision.gameObject.layer)) > 0) {
+            DistruggiProiettile();
+        }
+
+    }
+    
+    void DistruggiProiettile() {
+        Destroy(gameObject);
+    }
+
+}
---
+++ TheUltimateRagnarok/Assets/Scripts/RangedAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedAttack : MonoBehaviour
+{
+    [Header("Componenti")]
+    public Transform puntoSparo;
+    public GameObject proiettilePrefab;
+
+    [Header("Parametri")]
+    public KeyCode tastoSparo = KeyCode.F;
+    public float tempoRicarica = 0.0f;
+    private float prossimoSparo = 0.0f;
+
+    
+    void Update() {
+        
+        if(Input.GetKeyDown(tastoSparo) && Time.time >= prossimoSparo) {
+            Spara();
+            prossimoSparo = Time.time + tempoRicarica;
+        }
+
+    }
+
+
+    void Spara() {
+
+        GameObject colpo = Instantiate(proiettilePrefab, puntoSparo.position, Quaternion.identity); 
+
+        Proiettile scriptProiettile = colpo.GetComponent<Proiettile>();
+
+        if(scriptProiettile != null ) {
+
+            float direzione = Mathf.Sign(transform.localScale.x);
+            scriptProiettile.Lancia(direzione, gameObject);
+
+        }
+
+
+    }
+
+}
