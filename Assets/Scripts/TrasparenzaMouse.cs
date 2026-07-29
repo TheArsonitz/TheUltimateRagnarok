b2a4f3f26e05f00d53c65546a5861937c235aedd
Lorenzo Liseni <lorenzo1@MacBookAir.fritz.box>
2026-07-29 13:31:17 +0200

    non credo siano collegate le finestre fratello intanto ti pusho cio che ho

---
+++ Assets/Scripts/TrasparenzaMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TrasparenzaMouse : MonoBehaviour
+{
+    void Start()
+    {
+        // Questo comando dice a Unity di ignorare i pixel trasparenti
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
